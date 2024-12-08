54055c10877a1616e2d0d79596962cb4f726e917
blkcor <blkcor.dev@gmail.com>
2024-12-08 18:59:36 +0800

    chore: update scripts

--- packages/moten-ui/vite.config.ts
+++ packages/moten-ui/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue'
 import { createVuePlugin } from 'vite-plugin-vue2'
 import { resolve } from 'path'
+import { isVue2 } from 'vue-demi'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()],
+  plugins: [isVue2 ? createVuePlugin() : vue()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
